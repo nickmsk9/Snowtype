2afcc214bb8b938ee3af241fa5fac06c7cd5aa3c
Nick Sevalnev <Nickmsk9@icloud.com>
2025-11-17 15:22:16 +0300

    Initial Commit

---
+++ Snowtype/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Snowtype
+//
+//  Created by Никита Севальнев on 17.11.2025.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
